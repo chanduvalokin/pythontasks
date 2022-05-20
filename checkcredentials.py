daa61f7552146e40e59e826e565da660d0727c01
chanduvalokin <105814608+chanduvalokin@users.noreply.github.com>
2022-05-20 13:14:01 +0530

    Add files via upload

---
+++ checkcredentials.py
@@ -0,0 +1,10 @@
+username="chanduv"
+password="password"
+
+checkusername=input("Enter the username")
+checkpassword=input("Enter the password")
+
+if username==checkusername and password==checkpassword:
+    print("credentials are same")
+else:
+    print("invalid credentials:invalid username or password")
---
+++ printnums.py
@@ -0,0 +1,10 @@
+
+sum=0
+for n in range(1,21):
+    if n%2!=0 and n%3!=0:
+        print(n)
+        sum=sum+n
+print(f"Sum of multiples of b/w 1-20 is {sum}\n")
+
+
+
